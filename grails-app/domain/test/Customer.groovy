b1a0f1e713d604eff42fa51e52e6ca2f96398f19
David Brown <dave@tucanoo.com>
2022-10-24 22:36:12 +0200

    Experiment with 'NULL' String for defaultValue.

--- grails-app/domain/test/Customer.groovy
+++ grails-app/domain/test/Customer.groovy
@@ -1,10 +1,18 @@
 package test
 
+import org.hibernate.dialect.MySQL5Dialect
+
+import java.sql.Types
+
 class Customer {
 
     String name
 
+    static mapping = {
+        // Specifying the defaultValue of 'NULL' below is enough, although it's a String
+        name defaultValue: 'NULL'
+    }
     static constraints = {
-        nullable: true
+        name nullable: true
     }
 }
